a02bcfffe05dc1c5e3bf2ec2ce1e1e142ed21f4b
CPEJBTANA <jb.tana.cpe@gmail.com>
2024-09-26 16:07:05 +0800

    made this shit better

--- Script.js
+++ Script.js
@@ -9,3 +9,27 @@ function copyToClipboard(element) {
       console.error("Failed to copy: ", err);
     });
 }
+
+const images = document.querySelectorAll(".img");
+
+function clearActiveImage() {
+  images.forEach(function (image) {
+    image.classList.remove("active");
+  });
+}
+
+images.forEach(function (image, index) {
+  image.onclick = function () {
+    event.stopPropagation(); //important to not call the clearActiveImage() on every click
+    if (images[index].classList.contains("active")) {
+      images[index].classList.remove("active");
+    } else {
+      clearActiveImage(index);
+      images[index].classList.add("active");
+    }
+  };
+});
+
+window.addEventListener("click", () => {
+  clearActiveImage();
+});
